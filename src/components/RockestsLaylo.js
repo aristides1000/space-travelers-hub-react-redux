0d85588edfd40e321609989a45ea01963aedcee8
Aristides Jose Molina Perez <aristides1000@gmail.com>
2021-10-05 11:52:39 -0400

    Finish See components in the pages

---
+++ src/components/RockestsLaylo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Rocket = () => (
+  <div>
+    <h3>I am in Rockets Laylo component</h3>
+  </div>
+);
+export default Rocket;
--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Missions from './Missions';
 import MyProfile from './MyProfile';
 import Navbar from '../components/Navbar';
+import Rocket from '../components/RockestsLaylo';
 
 const Rockets = () => (
   <>
@@ -11,6 +12,7 @@ const Rockets = () => (
       <Route exact path="/">
         <div>
           <h3>I am in Rockets page</h3>
+          <Rocket />
         </div>
       </Route>
       <Route path="/missions">
