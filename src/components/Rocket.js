23adfd27209f95f6df0e7557d88d1d3bfef200af
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-10-06 18:39:57 +0500

    Add basic page layout

---
+++ src/components/Rocket.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+import { Card, Button } from 'react-bootstrap';
+import planet from '../media/planet.png';
+
+const styles = [
+  {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    marginBottom: '1.5rem',
+    alignItems: 'center',
+    padding: '2rem 1rem',
+    boxShadow: '0 0 1px 0',
+  },
+];
+
+const Rocket = () => (
+  <div className="rocket-container">
+    <Card style={styles[0]}>
+      <Card.Body>
+        <img className="rocket-img" src={planet} alt="planet" />
+      </Card.Body>
+      <Card.Body>
+        <Card.Title>Falcon 1</Card.Title>
+        <Card.Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Etiam dignissim ex vitae turpis luctus venenatis.
+          Aenean in lectus ut elit commodo lobortis quis nec nunc.
+          In quis ex augue. Cras rutrum,
+          dui condimentum gravida porta,
+        </Card.Text>
+        <Button variant="primary">Reserve Rocket</Button>
+      </Card.Body>
+    </Card>
+  </div>
+);
+
+export default Rocket;
+
+Rocket.propTypes = {
+  flickrImages: PropTypes.any,
+  rocketName: PropTypes.any,
+  description: PropTypes.any,
+}.isRequired;
