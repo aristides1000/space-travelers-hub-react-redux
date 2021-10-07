e5089f6196fef5b2bf9f50820ca11afd0361e542
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-10-08 01:15:00 +0500

    Display reserved rockets in My Profile

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -8,6 +8,7 @@ import RocketsList from '../components/RocketsList';
 
 function Rockets() {
   const { missionReducer } = useSelector((state) => state);
+  const allRockets = useSelector((state) => state.rockets.rockets);
 
   return (
     <>
@@ -17,7 +18,7 @@ function Rockets() {
           <RocketsList />
         </Route>
         <Route path="/missions" exact render={() => <Missions store={missionReducer} />} />
-        <Route path="/my-profile" render={() => <MyProfile missions={missionReducer} />} />
+        <Route path="/my-profile" render={() => <MyProfile missions={missionReducer} rocket={allRockets} />} />
       </Switch>
     </>
   );
